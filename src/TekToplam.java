2d2cf98d2a699a0c5434937d4c00429ae3bebaeb
uygar cankat <uygarcankat@sabanciuniv.edu>
2022-07-07 12:49:07 +0300

    first commit

---
+++ src/TekToplam.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+
+
+public class TekToplam {
+    public static void main(String[] args) {
+/*
+        Scanner input = new Scanner(System.in);
+        boolean isTrue = true;
+        int sum=0;
+        int number;
+        int k = 0 ;
+
+
+
+        do {
+            System.out.println("Lütfen bir sayı giriniz");
+            number = input.nextInt();
+            k++;
+            if (number < 0) {
+                isTrue = false;}
+
+            else {
+                System.out.println(number);
+
+                sum +=number;
+
+
+            }
+        }
+            while (isTrue) ;
+
+        System.out.println("sum:"+sum);
+        double avarage= sum/2;
+        System.out.println("avarage:"+avarage);
+
+
+        System.out.println("...........................................");
+*/
+
+        Scanner input = new Scanner(System.in);
+        boolean isTrue = true;
+        int sum=0;
+        int number;
+        int k = 0 ;
+
+        do {
+            System.out.println("Lütfen bir sayı giriniz");
+            number = input.nextInt();
+            k++;
+            if (number % 2 !=0 ) {
+                isTrue = false;}
+
+            else if((number % 2==0) || (number % 4==0) ) {
+                System.out.println(number);
+
+                sum +=number;
+
+
+            }
+        }
+        while (isTrue) ;
+
+        System.out.println("sum:"+sum);
+        double avarage= sum/2;
+        System.out.println("avarage:"+avarage);
+
+
+    }
+}
+
